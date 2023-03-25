434b3d5f630c54662c003c970840c1138f9df06b
vincent <vincentjaybano45@gmail.com>
2023-03-25 13:19:55 +0800

    resume

---
+++ js/custom.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$("#fullpage").fullpage({verticalCentered:!1,scrollingSpeed:600,autoScrolling:!1,css3:!0,navigation:!0,navigationPosition:"right"})}),$(function(){(new WOW).init(),$(".rotate").textrotator()});
